768014cd0754e07708dea3d135159d32b19aa1a9
Vladimir <adda37@yandex.ru>
2025-01-02 21:06:33 +0300

    last version

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ let city = document.querySelector('.weather__city')
 let temp = document.querySelector('.weather__temp')
 let icon = document.querySelector('.weather__img')
 let api = 'd47083b2cf007ead6de0f6fb9902daf3'
+let IPapi = 'at_BDy7TyouEuDcUa28z5Bi5DgrL3Kn5'
+
+function enableLocation(data) {
+    let lat = data.coords.latitude
+    let lon = data.coords.longitude
+    console.log(lat, lon);
+
+    getWeatherByCoords(lat, lon)
+}
+
+function disableLocation() {
+    // input.classList.add('dnone')
+    // error.classList.remove('dnone')
+    // weather.classList.add('dnone')
+    getCityByIP()
+}
+
+navigator.geolocation.getCurrentPosition(enableLocation, disableLocation);
 
 change.addEventListener('click', () => {
     input.classList.remove('dnone')
@@ -23,8 +41,8 @@ submit.addEventListener('click', (event) => {
 
     if (inputField.value != '') {
         console.log(inputField.value);
-        console.log(getWeather(inputField.value));
-        city.innerText = getWeather(inputField.value).weather[0].description + inputField.value
+        console.log(getWeatherByCityName(inputField.value));
+        city.innerText = getWeatherByCityName(inputField.value).weather[0].description + inputField.value
     }
 })
 
@@ -40,14 +58,9 @@ errorButton.addEventListener('click', () => {
     input.classList.remove('dnone')
 })
 
-async function getWeather(cityName) {
-    const geo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}`)
-    const data = await geo.json()
-    // console.log(data);
-    console.log(data.weather);
-    
+function renderContent(data) {
     if (data.weather != undefined) {
-        city.innerText = data.weather[0].main + ' in ' + inputField.value
+        city.innerText = data.weather[0].main + ' in ' + data.name
         temp.innerText = Math.round(data.main.temp - 273.15) + '℃'
         icon.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`)
     } else {
@@ -55,5 +68,41 @@ async function getWeather(cityName) {
         error.classList.remove('dnone')
         weather.classList.add('dnone')
     }
+}
+
+async function getWeatherByCityName(cityName) {
+    const geo = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}`)
+    const data = await geo.json()
+    // console.log(data);
+    console.log(data.weather);
+    renderContent(data)
+
+}
+
+async function getWeatherByCoords(lat, lon) {
+    const geo = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api}`)
+    const data = await geo.json()
+    renderContent(data)
+
+}
+
+async function getCityByIP() {
+    const getIP = await fetch('https://api64.ipify.org?format=json')
+    const IP = await getIP.json()
+    console.log(IP);
+
+    const geo = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${IPapi}&ipAddress=${IP.ip}`)
+    const data = await geo.json()
+    if (data.location != undefined) {
+        console.log(data.location);
+        let lan = data.location.lat
+        let lon = data.location.lng
+        getWeatherByCoords(lan, lon)
+    } else {
+        input.classList.add('dnone')
+        error.classList.remove('dnone')
+        weather.classList.add('dnone')
+    }
+
 
 }
